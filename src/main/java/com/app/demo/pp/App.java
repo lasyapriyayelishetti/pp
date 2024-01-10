237dd1ec8c79ad263f654486bcf2a3a616b6dc0f
lasyapriyayelishetty@gmail.com <lasyapriyayelishetty@gmail.com>
2024-01-10 22:49:01 +0530

    Demo for Team

---
+++ src/main/java/com/app/demo/pp/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.pp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
